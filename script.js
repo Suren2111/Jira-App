c4880a9e9e380aa1cea4ee098f1990525cfa88a2
Suren2111 <surendiran2111@gmail.com>
2022-04-14 12:36:17 +0530

    ticketdisplay

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 let addbtn=document.querySelector(".add-btn");
 let addmodel=true;
 let modelcont=document.querySelector(".model-cont");
+let maincont=document.querySelector(".main-cont");
+let textareacont=document.querySelector(".textarea-cont")
 addbtn.addEventListener("click",function(){
 //Display
     if(addmodel){
@@ -12,3 +14,26 @@ addbtn.addEventListener("click",function(){
     }
     addmodel=!addmodel;
 })
+textareacont.addEventListener("keydown",function(e){
+   //console.log(e);
+   let value=e.key;
+  // console.log(value);
+  if(value=='Enter'){
+      //console.log('Enter is pressed');
+      ticketdisplay(textareacont.value);
+      textareacont.value="";
+      modelcont.style.display="none";
+      addmodel=!addmodel;
+  }
+})
+
+function ticketdisplay(task){
+    let ticketcont=document.createElement('div');
+    ticketcont.setAttribute('class','ticket-cont');
+    ticketcont.innerHTML=` <div class="ticket-color"></div>
+    <div class="ticket-id"></div>
+    <div class="ticket-area">${task}</div>`;
+    maincont.appendChild(ticketcont);
+
+
+}
